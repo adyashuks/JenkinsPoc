38d344e2c64d89db571e9033f9e81437e9776098
adya.shukla <ashukla1@xpanxion.com>
2020-06-21 19:57:46 +0530

    Change test assertion

--- demo/src/test/java/com/jenkins/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/jenkins/demo/DemoApplicationTests.java
@@ -24,7 +24,7 @@ class DemoApplicationTests {
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+				.andExpect(content().string(equalTo("Greetings from Jenkins Demo!")));
 	}
 	
 	@Test
