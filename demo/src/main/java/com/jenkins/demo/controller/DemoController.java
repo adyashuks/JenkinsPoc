cbeeec94dfa9002e8b96e7750a930cb06c4eedd8
adya.shukla <ashukla1@xpanxion.com>
2020-06-21 20:01:35 +0530

    Change test assertion

--- demo/src/main/java/com/jenkins/demo/controller/DemoController.java
+++ demo/src/main/java/com/jenkins/demo/controller/DemoController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Jenkins Demo Controller!";
+		return "Greetings from Jenkins Demo!";
 	}
 }
